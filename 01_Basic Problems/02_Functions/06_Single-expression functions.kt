c8904b9f7fedc1f2495f35e4d16303cdc48dd8ef
Ouamira <anasouamira@gmail.com>
2025-09-26 22:54:06 +0100

    Created By Anas Ouamira

--- 01_Basic Problems/02_Functions/06_Single-expression functions.kt
+++ 01_Basic Problems/02_Functions/06_Single-expression functions.kt
@@ -1,16 +1,10 @@
 
 
 fun main() {
-    println(doubleBlock(5))   // Output: 10
-    println(doubleSingle(5))  // Output: 10
-    println(square(4))        // Output: 16
-    println(greet("Kotlin"))  // Output: Hello, Kotlin!
-    println(max(3, 7))        // Output: 7
+// Exercise 9 – Double Number
+fun double(n: Int) = n * 2
+
+
 }
 
-// ===========================================
-// Notes:
-// - Use single-expression functions for concise, readable code.
-// - Ideal for simple computations or one-line logic.
-// - For longer logic or multiple statements, use a regular block body.
-// ===========================================
+
